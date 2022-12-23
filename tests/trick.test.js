72e3974dd3167fb5d3f4d53ff4079c16578c864e
Siubaak <jimkwan.yau@gmail.com>
2022-12-23 15:22:54 +0800

    [chore] add trick tests

---
+++ tests/trick.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import vDebugger from '../src';
+
+describe('trick tests', () => {
+  it('redeclare in global scope normally', () => {
+    const run = vDebugger.debug(
+      'function a() {}\n' +
+      'var a;'
+    );
+    expect(run).toBeTruthy();
+
+    run();
+  });
+
+  // it('redeclare in function scope normally', () => {
+  //   const run = vDebugger.debug(
+  //     'function b() {\n' +
+  //     '  function a() {}\n' +
+  //     '  var a;\n' +
+  //     '}'
+  //   );
+  //   expect(run).toBeTruthy();
+
+  //   run();
+  // });
+});
